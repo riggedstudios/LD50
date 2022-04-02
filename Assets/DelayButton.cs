983c15ae6ed8f096fbd88d0519a2fa4b4c00901f
rigdonware <rigdon.ware@gmail.com>
2022-04-02 15:11:38 -0600

    Adding stopwatch and hand code and images

---
+++ Assets/DelayButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayButton : MonoBehaviour
+{
+	public GameObject mStopWatchObject;
+	StopWatch mStopWatch;
+    // Start is called before the first frame update
+    void Start()
+    {
+		mStopWatch = mStopWatchObject.GetComponent<StopWatch>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnMouseDown()
+	{
+		mStopWatch.IncreaseStopWatchTimer(1);
+	}
+}
---
+++ Assets/StopWatch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StopWatch : MonoBehaviour
+{
+	float mTimer = 1.0f;
+	Time mTotalTime;
+	GameObject mPointer;
+	public Text mTimerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+		mPointer = this.transform.GetChild(1).gameObject;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		mPointer.transform.localEulerAngles = Vector3.Lerp(mPointer.transform.localEulerAngles, new Vector3(0, 0, mTimer * 6), Time.deltaTime);
+		mTimer += Time.deltaTime;
+		if (mTimer >= 60)
+			mTimer = 60;
+		mTimerText.text = mTimer.ToString();
+	}
+
+	public void IncreaseStopWatchTimer(float amount)
+	{
+		mTimer += amount;
+		Debug.Log("Increasing Timer");
+	}
+}
